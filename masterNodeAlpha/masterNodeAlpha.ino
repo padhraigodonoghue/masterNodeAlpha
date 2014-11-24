74be7b4e24056d9cfe1ac18e77448558394629c4
padhraigodonoghue <0350788@studentmail.ul.ie>
2014-11-24 17:01:32 +0000

    5pm version
    more functionality added

--- masterNodeAlpha/masterNodeAlpha.ino
+++ masterNodeAlpha/masterNodeAlpha.ino
@@ -1,10 +1,16 @@
-int frameDuration      = 4200; // in milliseconds, i.e. 7 minutes
-int framesPerAct       = 9;    // number of "frames" in each "act" of composition ("act" is 1/3 of composition)
-int actsPerComposition = 3;    // number of "acts" in the composition
+int frameDuration      = 420000; // in milliseconds, i.e. 7 minutes
+int framesPerAct       = 9;      // number of "frames" in each "act" of composition ("act" is 1/3 of composition)
+int actsPerComposition = 3;      // number of "acts" in the composition
+int ruleDuration       = 5000    // in milliseconds
+int playbackBuffer     = 100     // in milliseconds
+
+long actDuration;
+int framesPerComposition;
 
 void setup()
 {
-  
+  actDuration = frameDuration * framesPerAct;
+  framesPerComposition = framesPerAct * actsPerComposition;
 }
 
 void loop()
@@ -27,12 +33,36 @@ void scheduler()
 // is it time to play anything or can serial port buffer be serviced?
 boolean priorityServiceSerial()
 {
-  checkTime();
-  
+ long wrapTime = millis() % actDuration;
+ 
+ // what rule number is coming up next
+ int ruleVicinity = (wrapTime / ruleDuration);
+ 
+ // if we are in range of playable rules
+ if (ruleVicinity < framesPerComposition)
+ {
+   // check if we are within the playback buffer range of playing a note
+   if ((wrapTime % ruleDuration) > (ruleDuration - playbackBuffer))
+   {
+     return false;
+   }
+ }
+ else
+ {
+   return true;
+ }
 }
 
-void checkTime()
+void serviceSerial()
 {
- long currentTime = millis();
- 
+  while(Serial.available())
+  {
+    // increment count on whatever frame for every byte in the serial port buffer
+    incrementer();
+  }
+}
+
+void incrementer()
+{
+  
 }
