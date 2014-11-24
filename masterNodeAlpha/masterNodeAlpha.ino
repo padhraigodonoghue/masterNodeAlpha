0cbe7e304ff4f8bc7179accf92769a34216dee51
padhraigodonoghue <0350788@studentmail.ul.ie>
2014-11-24 17:56:33 +0000

    6pm version
    2-dimensional array version

--- masterNodeAlpha/masterNodeAlpha.ino
+++ masterNodeAlpha/masterNodeAlpha.ino
@@ -1,16 +1,34 @@
-int frameDuration      = 420000; // in milliseconds, i.e. 7 minutes
-int framesPerAct       = 9;      // number of "frames" in each "act" of composition ("act" is 1/3 of composition)
-int actsPerComposition = 3;      // number of "acts" in the composition
-int ruleDuration       = 5000    // in milliseconds
-int playbackBuffer     = 100     // in milliseconds
+const int frameDuration      = 420000; // in milliseconds, i.e. 7 minutes
+const int framesPerAct       = 9;      // number of "frames" in each "act" of composition ("act" is 1/3 of composition)
+const int actsPerComposition = 3;      // number of "acts" in the composition
+const int ruleDuration       = 5000;   // in milliseconds
+const int playbackBuffer     = 100;    // in milliseconds
+
+int maxFrameTraffic          = 200;    // rough estimate; will be dynamically adjusted as max is exceeded
 
 long actDuration;
 int framesPerComposition;
+
+int act0[framesPerAct];
+int act1[framesPerAct];
+int act2[framesPerAct];
+int act3[framesPerAct];
+
+// 2-dimensional array of counts
+int superFrameArray[actsPerComposition + 1][framesPerAct];
 
 void setup()
 {
   actDuration = frameDuration * framesPerAct;
   framesPerComposition = framesPerAct * actsPerComposition;
+
+  for(int i = 0; i < actsPerComposition; i++)
+  {
+    for (int j = 0; j < framesPerAct; j++)
+    {
+      superFrameArray[i][j]= maxFrameTraffic;
+    }
+  }
 }
 
 void loop()
@@ -33,24 +51,24 @@ void scheduler()
 // is it time to play anything or can serial port buffer be serviced?
 boolean priorityServiceSerial()
 {
- long wrapTime = millis() % actDuration;
- 
- // what rule number is coming up next
- int ruleVicinity = (wrapTime / ruleDuration);
- 
- // if we are in range of playable rules
- if (ruleVicinity < framesPerComposition)
- {
-   // check if we are within the playback buffer range of playing a note
-   if ((wrapTime % ruleDuration) > (ruleDuration - playbackBuffer))
-   {
-     return false;
-   }
- }
- else
- {
-   return true;
- }
+  long actWrapTime = millis() % actDuration;
+
+  // what rule number is coming up next
+  int ruleVicinity = (actWrapTime / ruleDuration);
+
+  // if we are in range of playable rules
+  if (ruleVicinity < framesPerComposition)
+  {
+    // check if we are within the playback buffer range of playing a note
+    if ((actWrapTime % ruleDuration) > (ruleDuration - playbackBuffer))
+    {
+      return false;
+    }
+  }
+  else
+  {
+    return true;
+  }
 }
 
 void serviceSerial()
@@ -64,5 +82,26 @@ void serviceSerial()
 
 void incrementer()
 {
+  // increment
   
+  // look only at window of time equivalent to full composition and 1/3, i.e. 4 "acts"
+  long superCompositionWrapTime = (millis() % (actDuration * (actsPerComposition + 1)));
+  
+  int whichAct = (superCompositionWrapTime / actDuration;
+  int whichBin = (superCompositionWrapTime / frameDuration);
+  
+  superFrameArray[whichAct][whichAct]++;
 }
+
+void playComposition()
+{
+  // calculate delay
+  // delay
+  
+  long CompositionWrapTime = (millis() % (actDuration * (actsPerComposition)));
+  
+  // play note, i.e. send rule
+  playNote(
+
+}
+
