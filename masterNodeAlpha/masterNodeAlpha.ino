b0959bfba84e03231494de7a8984ed803292a381
padhraigodonoghue <0350788@studentmail.ul.ie>
2014-11-24 15:58:15 +0000

    4pm version
    added global variables; added priorityServiceSerial() function

--- masterNodeAlpha/masterNodeAlpha.ino
+++ masterNodeAlpha/masterNodeAlpha.ino
@@ -1,5 +1,10 @@
+int frameDuration      = 4200; // in milliseconds, i.e. 7 minutes
+int framesPerAct       = 9;    // number of "frames" in each "act" of composition ("act" is 1/3 of composition)
+int actsPerComposition = 3;    // number of "acts" in the composition
+
 void setup()
 {
+  
 }
 
 void loop()
@@ -9,8 +14,7 @@ void loop()
 
 void scheduler()
 {
-  prioritiser();
-  if (priorityServiceSerial == true)
+  if (priorityServiceSerial() == true)
   {
     serviceSerial();
   }
@@ -18,4 +22,17 @@ void scheduler()
   {
     playComposition();
   }
+}
+
+// is it time to play anything or can serial port buffer be serviced?
+boolean priorityServiceSerial()
+{
+  checkTime();
+  
+}
+
+void checkTime()
+{
+ long currentTime = millis();
+ 
 }
